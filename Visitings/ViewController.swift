e075afe7a52e1612d3d60b661f824317d2f85327
Glen <glenyi81@gmail.com>
2014-09-10 22:01:53 -0400

    Initial Commit

---
+++ Visitings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Visitings
+//
+//  Created by Glen Yi on 2014-09-10.
+//  Copyright (c) 2014 On The Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
